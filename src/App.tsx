db6ff64ee836e81624b403d979bbfee4524a700a
Storm <Sinders96@live.com>
2023-09-09 16:15:12 -0500

    List order for Products

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,79 @@
-
 type Product = {
-  name: string
-  cost: number
-  inventory: number
+  name: string;
+  cost: number;
+  inventory: number;
 };
 
 enum Money {
-  penny = .01,
-  nickel = .05,
-  quarter= .25,
+  penny = 0.01,
+  nickel = 0.05,
+  quarter = 0.25,
   dollar = 1,
   five = 5,
   ten = 10,
-  twenty = 20
+  twenty = 20,
 }
 //Implement Credit Cards
 
-
-const items: Product[] = [
+const Products = [
   {
-    name: 'Coke',
-    cost: 1.50,
-    inventory: 15
+    name: "Coke",
+    cost: 1.5,
+    inventory: 15,
   },
 
   {
-    name: 'Sprite',
+    name: "Sprite",
     cost: 1.25,
-    inventory: 10
+    inventory: 10,
   },
 
   {
-    name: 'Water',
-    cost: 1.50,
-    inventory: 20
+    name: "Water",
+    cost: 1.5,
+    inventory: 20,
   },
 
   {
-    name: 'Powdered_Donuts',
-    cost: 2.00,
-    inventory: 10
+    name: "Powdered Donuts",
+    cost: 2.0,
+    inventory: 10,
   },
 
   {
-    name: 'Cheezits',
+    name: "Cheezits",
     cost: 1.75,
-    inventory: 15
+    inventory: 15,
   },
 
   {
-    name: 'Doritos',
-    cost: 1.50,
-    inventory: 10
-  }
-]
-
-function Selection(){
-
-}
-
-console.log(items)
-console.log(items.map(soda => soda.name))
+    name: "Doritos",
+    cost: 1.5,
+    inventory: 10,
+  },
+];
 
 
+function Selection() {
+
+  const productNames = Products.map(product => <li>{product.name}</li>);
+  const productCosts = Products.map(product => <li>{product.cost}</li>);
+
+  return(
+    <div>
+
+    <h1>Virtual Vending Machine</h1>
+    <ul>{productNames}</ul>
+    
+
+    </div>
+    
+)}
 
 
 
+//console.log(items);
+//console.log(items.map((soda) => soda.name));
 
 
-
-
-function App(){
-  return null
-}
-export default App
+export default Selection;
